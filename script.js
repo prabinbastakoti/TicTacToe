e1fe465c1f50d44cdf3a553efee8c7ba3459f564
Prabin Bastakoti <101822704+PrabinBastakoti@users.noreply.github.com>
2023-06-28 22:51:28 +0545

    Design Front Page

---
+++ script.js
@@ -0,0 +1,15 @@
+
+// ====================Create Player====================
+
+const player = (sign) => {
+    return sign;
+};
+
+
+// ====================GameBoard====================
+const gameBoard = (() => {
+    // const board = new Array(9);
+    const board = ["1","2","3","4","5","6","7","8","9"];
+    console.log(board);
+})();
+
